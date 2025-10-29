54dae6c5a74bfc2779c8766e9179ca6eea023050
Beilong Tang <btang5@pintabian.ece.ncsu.edu>
2025-10-29 09:47:28 -0400

    update

--- dataset/cifar10.py
+++ dataset/cifar10.py
@@ -3,6 +3,7 @@ import torchvision
 import torchvision.transforms as transforms
 import lightning as L
 
+
 class Cifar10DataModule(L.LightningDataModule):
     def __init__(self, batch_size):
         super().__init__()
@@ -11,16 +12,24 @@ class Cifar10DataModule(L.LightningDataModule):
 
     def setup(self, stage):
         transform = transforms.Compose([transforms.ToTensor()])
-        if stage=="fit":
-            self.trainset = torchvision.datasets.CIFAR10(
-                root="./data", train=True, download=True, transform=transform
-            )
-            self.valset = torchvision.datasets.CIFAR10(
-                root="./data", train=False, download=True, transform=transform
-            )
+        self.trainset = torchvision.datasets.CIFAR10(
+            root="./data", train=True, download=True, transform=transform
+        )
+        self.valset = torchvision.datasets.CIFAR10(
+            root="./data", train=False, download=True, transform=transform
+        )
 
     def train_dataloader(self):
-        return DataLoader(self.trainset, batch_size=self.batch_size, shuffle=True, num_workers=4)
-    
+        return DataLoader(
+            self.trainset, batch_size=self.batch_size, shuffle=True, num_workers=4
+        )
+
     def val_dataloader(self):
-        return DataLoader(self.valset, batch_size=self.batch_size, shuffle=True, num_workers=4)
+        return DataLoader(
+            self.valset, batch_size=self.batch_size, shuffle=True, num_workers=4
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.trainset, batch_size=self.batch_size, shuffle=True, num_workers=4
+        )
